ea178a08987d6b2428f23411cb132dc4349a84b3
naledi-dikgale <naledi22khosi@gmail.com>
2023-06-19 19:07:08 +0200

    Implement requested changes

--- decorator.rb
+++ decorator.rb
@@ -1,6 +1,8 @@
 require_relative 'nameable'
 
 class Decorator < Nameable
+  attr_accessor :nameable
+
   def initialize(nameable)
     super()
 
--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -2,6 +2,11 @@ require_relative 'decorator'
 
 class TrimmerDecorator < Decorator
   def correct_name
-    super[0...10]
+    trimmed_name = super
+    if trimmed_name.length > 10
+      trimmed_name[0..9]
+    else
+      trimmed_name
+    end
   end
 end
