d9571cebee223bcd921d0a20faca71d82d17d5a9
naledi-dikgale <naledi22khosi@gmail.com>
2023-06-21 14:57:51 +0200

    Update person.rb file

--- person.rb
+++ person.rb
@@ -7,7 +7,7 @@ class Person < Nameable
   attr_accessor :name, :age, :rentals, :parent_permission
   attr_reader :id
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name: 'Unknown', parent_permission: true)
     super()
 
     @id = rand(1..1000)
