c6677ce31f2d4d0d574be0238abb60b0d94b4389
naledi-dikgale <naledi22khosi@gmail.com>
2023-06-21 14:58:25 +0200

    Update student.rb

--- student.rb
+++ student.rb
@@ -3,8 +3,8 @@ require_relative 'person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
-    super(name, age, parent_permission)
+  def initialize(age, name, classroom, parent_permission: true)
+    super(age, name: name, parent_permission: parent_permission)
     @classroom = classroom
   end
 
