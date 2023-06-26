b8fb412fc49e2ec18d6f61a468b4dd6eb85c251e
naledi-dikgale <naledi22khosi@gmail.com>
2023-06-26 10:10:55 +0200

    Create presentation.rb module

---
+++ presentations.rb
@@ -0,0 +1,12 @@
+module Presentations
+  # def presentations
+  def welcome_message
+    puts 'Welcome to School Library App!'
+  end
+
+  def farewell_message
+    puts 'Thank you for using this app!'
+    exit
+  end
+  # end
+end
