4bb332020b102b2dcfb7fae27a8122d9fab2c8f4
naledi-dikgale <naledi22khosi@gmail.com>
2023-06-27 15:37:11 +0200

    Update rental.rb file

--- rental.rb
+++ rental.rb
@@ -1,3 +1,17 @@
+# class Rental
+#   attr_accessor :date, :book, :person
+
+#   def initialize(date, book, person)
+#     @date = date
+#     @book = book
+#     @person = person
+#     person.rentals << self
+#     book.rentals << self
+#   end
+# end
+
+require 'json'
+
 class Rental
   attr_accessor :date, :book, :person
 
@@ -7,5 +21,22 @@ class Rental
     @person = person
     person.rentals << self
     book.rentals << self
+  end
+
+  def to_json(*args)
+    {
+      date: @date,
+      book: @book.to_json,
+      person: @person.to_json
+    }.to_json(*args)
+  end
+
+  def self.from_json(json)
+    data = JSON.parse(json)
+    new(
+      data['date'],
+      Book.from_json(data['book']),
+      Person.from_json(data['person'])
+    )
   end
 end
