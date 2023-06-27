a0b9773f83d9cce6e4f0c6eecc4a67358d3b59fc
naledi-dikgale <naledi22khosi@gmail.com>
2023-06-27 15:37:36 +0200

    Update save-data.rb file

--- Save-data.rb
+++ Save-data.rb
@@ -1,4 +1,4 @@
-require_relative 'store_files' 
+require_relative 'store-files'
 require 'json'
 
 class SaveData
@@ -9,29 +9,28 @@ class SaveData
     @rentals = 'rental.json'
   end
 
-    def save_books(title, author)
-         books = @storage.file_read(@books) if File.exist?(@books)
-        books = []
-        book= {
-            title: title,
-            author: author,
-            }
-        books.push(book)
-        @storage.file_write(@books, books)
-    end
+  def save_books(title, author)
+    books = @storage.file_read(@books) if File.exist?(@books)
+    books = []
+    book = {
+      title: title,
+      author: author
+    }
+    books.push(book)
+    @storage.file_write(@books, books)
+  end
 
-    def save_person(person)
-        Person = @storage.file_read(@person) if File.exist?(@person)
-        person = []
-        Person.push(person)
-        @storage.file_write(@person, person)
-    end
+  def save_person(person)
+    people = @storage.file_read(@people) if File.exist?(@people)
+    people = []
+    people.push(person)
+    @storage.file_write(@people, people)
+  end
 
-    def save_rentals(rental)
-        rentals = []
-        rental = @storage.file_read(@rentals) if File.exist?(@rentals)
-        rentals.push(rental)
-        @storage.file_write(@rentals, rentals)
-    end
-
+  def save_rentals(rental)
+    rentals = []
+    # rental = @storage.file_read(@rentals) if File.exist?(@rentals)
+    rentals.push(rental)
+    @storage.file_write(@rentals, rentals)
+  end
 end
