01530948628bfa08068c20fec7bced0cce2af6d0
naledi-dikgale <naledi22khosi@gmail.com>
2023-06-27 21:39:20 +0200

    Update store_files.rb file

--- store_files.rb
+++ store_files.rb
@@ -1,10 +1,19 @@
-class Storage
-  def file_read(filename)
-    file = File.read(filename)
-    JSON.parse(file)
+require 'json'
+def read_file(file)
+  if File.exist?(file)
+    if File.empty?(file)
+      []
+    else
+      file_data = File.read(file)
+      JSON.parse(file_data)
+    end
+  else
+    []
   end
+end
 
-  def file_write(filename, data)
-    File.write(filename, JSON.generate(data))
-  end
+def write_file(file, content)
+  data = content.map(&:to_hash)
+  json = JSON.generate(data)
+  File.write(file, json)
 end
