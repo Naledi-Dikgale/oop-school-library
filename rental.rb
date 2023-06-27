4bf210188b340041fd3eb6c650e29a382649deeb
naledi-dikgale <naledi22khosi@gmail.com>
2023-06-27 21:38:47 +0200

    Update rental.rb file

--- rental.rb
+++ rental.rb
@@ -1,42 +1,21 @@
-# class Rental
-#   attr_accessor :date, :book, :person
-
-#   def initialize(date, book, person)
-#     @date = date
-#     @book = book
-#     @person = person
-#     person.rentals << self
-#     book.rentals << self
-#   end
-# end
-
-require 'json'
-
 class Rental
   attr_accessor :date, :book, :person
 
   def initialize(date, book, person)
+    @id = Random.rand(1..1000)
     @date = date
     @book = book
     @person = person
-    person.rentals << self
-    book.rentals << self
+    # person['rentals'] << self
+    # book['rentals'] << self
   end
 
-  def to_json(*args)
+  def to_hash
     {
+      id: @id,
       date: @date,
-      book: @book.to_json,
-      person: @person.to_json
-    }.to_json(*args)
-  end
-
-  def self.from_json(json)
-    data = JSON.parse(json)
-    new(
-      data['date'],
-      Book.from_json(data['book']),
-      Person.from_json(data['person'])
-    )
+      book: @book,
+      person: @person
+    }
   end
 end
