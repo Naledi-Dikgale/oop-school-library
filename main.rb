4471b336a580618e95874b16e7cc9209d57cb1ed
naledi-dikgale <naledi22khosi@gmail.com>
2023-06-27 21:37:00 +0200

    Update main.rb file

--- main.rb
+++ main.rb
@@ -1,33 +1,3 @@
-# require_relative 'app'
-# require_relative 'ui_methods'
-# require_relative 'options'
-# require_relative 'presentations'
-
-# class Main
-#   include Options
-#   include UIMethods
-#   extend Presentations
-
-#   def initialize
-#     @app = App.new
-#   end
-
-#   def run
-#     self.class.welcome_message
-#     loop do
-#       show_menu
-#       selected = receive_input
-#       break if selected == 7
-
-#       select_option(selected, @app)
-#     end
-#     self.class.farewell_message
-#   end
-# end
-
-# main = Main.new
-# main.run
-
 require_relative 'app'
 require_relative 'ui_methods'
 require_relative 'options'
@@ -39,10 +9,7 @@ class Main
   extend Presentations
 
   def initialize
-    books = []
-    person = []
-    rentals = []
-    @app = App.new(books, person, rentals)
+    @app = App.new
   end
 
   def run
@@ -55,7 +22,6 @@ class Main
       select_option(selected, @app)
     end
     self.class.farewell_message
-    @app.save_data
   end
 end
 
