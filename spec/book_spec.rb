893f545b28390dd4778382200a2b1bdc02d69fb0
saluumaa <salumahassan02@gmail.com>
2023-06-28 12:15:14 +0300

    Fix the errors of the book test

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,42 +1,21 @@
-# require_relative '../book'
-# require_relative '../rental'
-# require_relative '../person'
+require_relative '../book'
 
-# describe Book do
-#   let(:title) { 'Book Title' }
-#   let(:author) { 'Book Author' }
-#   let(:person) { Person.new(25, true, 'John Doe') }
-#   let(:date) { '2023-06-28' }
 
-#   describe '#initialize' do
-#     it 'sets the title, author, and initializes an empty array of rentals' do
-#       book = Book.new(title, author)
-#       expect(book.title).to eq(title)
-#       expect(book.author).to eq(author)
-#       expect(book.rentals).to be_empty
-#     end
-#   end
+describe Book do
+    before :each do
+        @book = Book.new "Something", "Someone"
+    end
 
-#   describe '#add_rental' do
-#     it 'creates a new Rental object and adds it to the rentals array' do
-#       book = Book.new(title, author)
-#       book.add_rental(person, date)
-#       expect(book.rentals.size).to eq(1)
-#       expect(book.rentals.first).to be_a(Rental)
-#       expect(book.rentals.first.date).to eq(date)
-#       expect(book.rentals.first.book).to eq(book)
-#       expect(book.rentals.first.person).to eq(person)
-#     end
-#   end
+    it "takes two parameters and returns a Book object" do
+        expect(@book).to be_instance_of Book
+    end
+    
+    it "has two attributes: title and author" do
+        expect(@book).to have_attributes(:title => "Something", :author => "Someone")
+    end
 
-#   describe '#to_hash' do
-#     it 'returns a hash representation of the book' do
-#       book = Book.new(title, author)
-#       expected_hash = {
-#         title: title,
-#         author: author
-#       }
-#       expect(book.to_hash).to eq(expected_hash)
-#     end
-#   end
-# end
+    it "has a method add_rental" do
+        expect(@book).to respond_to(:add_rental)
+    end
+    
+end
